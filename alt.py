f6e47fbdf1c720e49ee28e1efaeadf121b2c39f9
Rifaz Nahiyan <rifazn@protonmail.com>
2021-05-02 05:59:48 +0600

    Re-implementing menu...

--- alt.py
+++ alt.py
@@ -5,8 +5,11 @@ from tabulate import tabulate
 from argparse import ArgumentParser
 import sys, json, gzip, requests, subprocess
 
-def fzf(subs_list):
+def _fzf(subs_list):
     return iterfzf(subs_list)
+
+def _menu(subs_list):
+    return 'asd'
 
 def print_json(subs):
     print(subs)
@@ -40,10 +43,8 @@ def get_subs(moviename):
 
     data = response.json()
 
-    subs = [
-        [sub['MovieName'], sub['SubFileName'], sub['SubDownloadLink'],
-         sub['Score']] for sub in data
-    ]
+    keys = ['MovieName', 'SubFileName', 'SubDownloadLink', 'Score']
+    subs = [{key: sub[key] for key in keys} for sub in data]
 
     return subs
 
@@ -54,7 +55,7 @@ def _get_cli_args():
     parser.add_argument('moviename')
 
     # The optional, but essental, 'options'
-    parser.add_argument('-m', '--menu', default=fzf,
+    parser.add_argument('-m', '--menu', action='store', default=_fzf,
         help='Use a menu program like dmenu, bemenu, etc.')
 
     # All the related to output
@@ -70,8 +71,17 @@ def _get_cli_args():
 
 
 if __name__ == "__main__":
+    # Parse cli arguments
     args = _get_cli_args()
-    # args.menu(args.moviename)
-    choice = args.menu('hi fzf'.split())
-    print(choice)
 
+    # Get subtitles from the REST apis
+    subs_dict_list = get_subs(args.moviename)
+    subs_dict_enum = enumerate(subs_dict_list)
+
+    # Format the dictionary items to be readable
+    _filter = lambda i, sub: [i, sub['MovieName'], sub['SubFileName'], sub['Score']]
+    subs_list = ['{:<3} | {:10.10} | {:<50.50} | {:<3.2}'
+                 .format(*_filter(idx, sub)) for idx, sub in subs_dict_enum]
+
+    # Let user choose
+    print(args.menu(subs_list))
